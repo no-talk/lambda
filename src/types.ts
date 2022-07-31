9ab8ae8e913714456ed26ae24dbed1b9dce1f0e1
오진수 <ohjinsu@codersproduct.com>
2022-07-31 13:59:56 +0900

    Redefine the body type any

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,5 @@ export type Lambda<Input, Output> = (input: Input) => Promise<Output>;
 export type ResponseData = {
   statusCode?: number;
   headers?: Record<string, string>;
-  body?: Record<string, unknown>;
+  body?: any;
 };
