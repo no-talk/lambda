2105bec0e69ceefbaf8773017ce7c22015a1a825
오진수 <ohjinsu@codersproduct.com>
2022-08-16 11:16:54 +0900

    Fix the error handling of authorizer

--- src/adapter/authorizer.ts
+++ src/adapter/authorizer.ts
@@ -1,34 +1,22 @@
 import { calculateRequest, calculateResponse } from "@notalk/core";
-import { APIGatewayRequestAuthorizerHandler } from "aws-lambda";
+import { APIGatewayRequestAuthorizerEvent, Callback, Context } from "aws-lambda";
 import { report } from "../common/logger";
 import { requestReducer } from "../reducers";
 import { Class, Lambda } from "../types";
 
 export const authorizerAdapter =
   <T, K>(request: Class<T>, response: Class<K>) =>
-  (lambda: Lambda<T, K>): APIGatewayRequestAuthorizerHandler =>
-  async (event) => {
+  (lambda: Lambda<T, K>) =>
+  async (event: APIGatewayRequestAuthorizerEvent, _: Context, callback: Callback) => {
     try {
       const input = calculateRequest({}, event, requestReducer, request);
 
       const output = await lambda(input);
 
-      const { principalId, ...responseContext } = calculateResponse(output as any, event, requestReducer, response);
+      const { principalId, ...context } = calculateResponse(output as any, event, requestReducer, response);
 
       if (!principalId) {
-        return {
-          principalId: "none",
-          policyDocument: {
-            Version: "2012-10-17",
-            Statement: [
-              {
-                Action: "execute-api:Invoke",
-                Effect: "Error",
-                Resource: event.methodArn,
-              },
-            ],
-          },
-        };
+        return callback("Error :: Princial id is not provided");
       }
 
       return {
@@ -43,23 +31,11 @@ export const authorizerAdapter =
             },
           ],
         },
-        context: responseContext,
+        context,
       };
     } catch (error) {
       report("Exception :: ", error, event);
 
-      return {
-        principalId: "none",
-        policyDocument: {
-          Version: "2012-10-17",
-          Statement: [
-            {
-              Action: "execute-api:Invoke",
-              Effect: "Unauthorized",
-              Resource: event.methodArn,
-            },
-          ],
-        },
-      };
+      callback("Unauthorized");
     }
   };
